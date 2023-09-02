d565c80e6d1b932adaf91ca61e467542574f6570
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-02 22:24:20 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -8,24 +8,26 @@ import React from 'react';
 
 class App extends React.Component {
 
-constructor(props){
-  super(props);
-  this.state = {
-    count: 0,
-    someKey: false
-  };
+state = {
+  count: 0
 }
 
 handleClick = () => {
   this.setState({count: this.state.count + 1});
 }
 
+handleClick2 = () => {
+  this.setState((previousState) => ({count: previousState.count + 1}))
+  this.setState((previousState) => ({count: previousState.count + 1}))
+  this.setState((previousState) => ({count: previousState.count + 1}))
+}
+
   render(){
     return (
       <div className="App">
        
-
         <button onClick={this.handleClick}>{this.state.count}</button>
+        <button onClick={this.handleClick2}>{this.state.count}</button>
       </div>
     );
   }
