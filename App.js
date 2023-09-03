3bcb3411f1fc5cb58884eeead05fe272f70c5594
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-03 12:14:15 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -8,34 +8,36 @@ import React from 'react';
 
 class App extends React.Component {
 
-  constructor(){
-    super();
-
-    this.state = {
-      count: 0
-    }
-
-    this.handleClickMinus = this.handleClickMinus.bind(this);
+  state = {
+    posts: [],
+    loading: true,
+    comments: []
   }
 
-
-handleClickPlus = () => {
-  this.setState({count: this.state.count + 1})
+componentDidMount(){
+  //console.log('componentDidMount');
+  fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(response => response.json())
+  .then(data => this.setState({posts: data, loading: false}))
+  
 }
 
-handleClickMinus(){
-  this.setState({count: this.state.count - 1})
+componentDidUpdate(){
+  console.log('updated');
+  // fetch('/meta');
+}
+
+componentWillUnmount(){
+
 }
 
 
   render(){
     return (
-      <div className="App">
-       
-        <button onClick={this.handleClickPlus}> Plus </button>
-        <p>{this.state.count}</p>
-        <button onClick={this.handleClickMinus}> Minus </button>
-        
+      <div className="App">   
+        {this.state.loading ? <h3>Loading...</h3> : <h3>
+          {this.state.posts.length} was loaded
+        </h3> }
       </div>
     );
   }
